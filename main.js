532798b84d44eca173a91121bdcd304d9141d168
manugp1994 <manu0970@hotmail.com>
2018-03-07 13:37:54 +0100

    Pictures_to_github

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 //SET MAP
 
+var margin = { top: 0, right: 0, bottom: 0, left: 0 },
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .append('g')
+  .attr('class', 'map');
+
 var format = d3.format(",");
 
 // Set tooltips
@@ -10,30 +21,17 @@ var tip = d3.tip()
     return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Population: </strong><span class='details'>" + format(d.population) + "</span>";
   })
 
-var margin = { top: 0, right: 0, bottom: 0, left: 0 },
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+svg.call(tip);
 
 var color = d3.scaleThreshold()
   .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000])
   .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)", "rgb(8,81,156)", "rgb(8,48,107)", "rgb(3,19,43)"]);
-
-var path = d3.geoPath();
-
-var svg = d3.select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .append('g')
-  .attr('class', 'map');
 
 var projection = d3.geoMercator()
   .scale(130)
   .translate([width / 2, height / 1.5]);
 
 var path = d3.geoPath().projection(projection);
-
-svg.call(tip);
 
 // COMBOBOX to pick year
 
